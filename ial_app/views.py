297d7acc8f9eaf0da17efb1ad2a227c1ff5c42e5
Filipe Machado <fmaachadoo@gmail.com>
2022-07-04 19:02:54 -0300

    dinamically display module on page

--- ial_app/views.py
+++ ial_app/views.py
@@ -9,13 +9,27 @@ def index(request):
 
 def modulo(request, module):
     requested_module = Module.objects.get(number=module)
-    topics = Topic.objects.filter(fk_module=module)
+    topics = Topic.objects.filter(fk_module=requested_module)
     lessons = Lesson.objects.filter(fk_topic__in=topics)
     exercises = ExerciseList.objects.filter(fk_topic__in=topics)
     data = {
         'module': requested_module,
-        'topics': topics,
-        'lessons': lessons,
-        'exercises': exercises
+        'topics': [
+            parse_topic(topic, lessons, exercises, index) for index, topic in enumerate(topics)
+        ]
     }
     return render(request, 'modulo.html', data)
+
+def parse_topic(topic, lessons, exercises, index):
+    return {
+        'status': 'active' if index == 0 else '',
+        'item': topic,
+        'lessons': filter_lessons(topic, lessons),
+        'exercises': filter_exercises(topic, exercises)
+    }
+
+def filter_lessons(topic, lessons):
+    return [lesson for lesson in lessons if lesson.fk_topic.id == topic.id]
+
+def filter_exercises(topic, exercises):
+    return list(filter(lambda x: x.fk_topic.id == topic.id, exercises))
