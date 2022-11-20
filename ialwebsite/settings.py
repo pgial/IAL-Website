8621c6149bb77c61daa713a964f496eb9dee7665
fmaachadoo <filipe__machado@hotmail.com>
2022-11-20 16:53:26 -0300

    feat(django settings): configure database to use environment variables

--- ialwebsite/settings.py
+++ ialwebsite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +79,23 @@ WSGI_APPLICATION = 'ialwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+print(os.getenv('ELEPHANT_DB_HOST'))
+print(os.getenv('ELEPHANT_DB_HOST'))
+print(os.getenv('ELEPHANT_DB_HOST'))
+print(os.getenv('ELEPHANT_DB_HOST'))
+print(os.getenv('ELEPHANT_DB_HOST'))
+print(os.getenv('ELEPHANT_DB_HOST'))
+print(os.getenv('ELEPHANT_DB_HOST'))
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('ELEPHANT_DB_NAME'),
+        'USER': os.getenv('ELEPHANT_DB_USER'),
+        'PASSWORD': os.getenv('ELEPHANT_DB_PASSWORD'),
+        'HOST': os.getenv('ELEPHANT_DB_HOST'),
+        'PORT': os.getenv('ELEPHANT_DB_PORT'),
     }
 }
 
