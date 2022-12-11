f80945b0b4a3375b2d494b9e38d608410c2db600
fmaachadoo <filipe__machado@hotmail.com>
2022-12-11 06:30:58 -0300

    fix: fixes static root

--- ialwebsite/settings.py
+++ ialwebsite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "ial_app/static/"
 
 STATIC_URL = '/static/'
 
