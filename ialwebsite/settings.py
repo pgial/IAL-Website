7916894c798e26c0ff54bae4c0e2d5c0646956a3
fmaachadoo <filipe__machado@hotmail.com>
2023-07-18 00:10:07 -0300

    chore: secure secret key

--- ialwebsite/settings.py
+++ ialwebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wsb&k=rgov-e=z1ttb5-vzj-lptcpf&$h@x^j==7mj4nq7@(jv'
+SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
